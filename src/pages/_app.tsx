dd0a2069f331d6eec31f7305e85f0dc79c01f6b1
mikecebul <mikecebul@gmail.com>
2022-08-04 09:28:34 -0400

    default Dark Mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ import { useState } from "react";
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
 
-  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("dark");
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
 
